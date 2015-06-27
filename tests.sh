57f1c786809e70fa43976da3d35abacea4a48882
Meemaw <totalfr33k@gmail.com>
2015-06-27 14:34:55 +0200

    All tests

--- tests.sh
+++ tests.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -e
 
+cd Assigment0 && make test
 cd Assigment1 && make test
+cd Assigment2 && make test
+cd Assigment3 && make test
+cd Assigment4 && make test
+cd Assigment5 && make test
+cd Assigment6 && make test
+cd Assigment7 && make test
+cd Assigment8 && make test
+cd Assigment9 && make test
+cd Assigment10 && make test
