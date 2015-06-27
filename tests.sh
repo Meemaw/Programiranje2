4ced2f7b5c4be665d6a14c93deffa0d4b7651340
Meemaw <totalfr33k@gmail.com>
2015-06-27 14:37:35 +0200

    tests changed

--- tests.sh
+++ tests.sh
@@ -1,13 +1,6 @@
 #!/bin/bash -e
 
-cd Assigment0 && make test
-cd Assigment1 && make test
-cd Assigment2 && make test
-cd Assigment3 && make test
-cd Assigment4 && make test
-cd Assigment5 && make test
-cd Assigment6 && make test
-cd Assigment7 && make test
-cd Assigment8 && make test
-cd Assigment9 && make test
-cd Assigment10 && make test
+for i in {0..10}
+do
+	(cd Assigment$i && make test)
+done
