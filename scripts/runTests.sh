4c4b5d00367bbc7cb61a0022e90f063b4ea1241b
Meemaw <totalfr33k@gmail.com>
2015-06-27 15:14:12 +0200

    Makefile changes

---
+++ scripts/runTests.sh
@@ -0,0 +1,11 @@
+ID=$1
+NUMINPUTS=$(ls tests/ | grep input | wc -l)
+
+
+echo "=== TESTING: ==="
+for (( i=1; i<=$NUMINPUTS; i++ ))
+do
+	./$ID < ./tests/input-$i > result-$i
+	if diff -w -B result-$i ./tests/output-$i ; then echo "$i - correct"; else exit 1; fi
+done
+
