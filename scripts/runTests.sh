d2275e2941f757561d1aa5ff24b1eae2c5f55188
Meemaw <totalfr33k@gmail.com>
2015-06-27 15:18:39 +0200

    fix loop

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -3,7 +3,7 @@ NUMINPUTS=$(ls tests/ | grep input | wc -l)
 
 
 echo "=== TESTING: ==="
-for (( i=1; i<=$NUMINPUTS; i++ ))
+for i in $(seq 1 $NUMINPUTS);
 do
 	./$ID < ./tests/input-$i > result-$i
 	if diff -w -B result-$i ./tests/output-$i ; then echo "$i - correct"; else exit 1; fi
